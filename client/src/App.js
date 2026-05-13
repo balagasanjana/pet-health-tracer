f8cb3410aad727bd39e5251f4c616c1c28f5b8e2
BALAGASANJANA <balagasanjana11@gmail.com>
2026-05-13 21:12:44 +0530

    Fix deployment issues and update backend URL

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import Signup from "./pages/Signup";
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
